eb7add905413e7ade8f0464123b4efa64caba9e4
Raja Sabiq <61377558+RajaSabiq@users.noreply.github.com>
2022-11-30 01:20:45 +0500

    updated

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ require('./conf');
 const app = express();
 app.use(express.json());
 app.use(cors());
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.status(200).send({
