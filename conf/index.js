54cfeb3fb20b539dd57cfc8030f6a4b1709ccca4
Raja Sabiq <61377558+RajaSabiq@users.noreply.github.com>
2022-12-07 00:41:26 +0500

    updated connection string for production

--- conf/index.js
+++ conf/index.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb+srv://RajaSabiq:Joker170@cluster0.ntvkl02.mongodb.net/demo?retryWrites=true&w=majority'
+  'mongodb+srv://systemaccount:Passw0rd@localhost/zencoach_dev?retryWrites=true&w=majority'
 );
